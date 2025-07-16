7999589b5d1dab976d8ab9598432b6e9850db97a
NalinPriyankara <nalinpriyankara033@gmail.com>
2025-07-16 22:16:53 +0530

    frontend

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,25 @@ import express, { request } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 //middleware for parsing request body
 app.use(express.json());
+
+//middleware for handling CORS POLICY
+//option 1: Allow all origins with default of cors(*)
+app.use(cors());
+
+//option 2: Allow custom origins
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type']
+//     })
+// );
 
 app.get('/', (req, res) => {
     console.log(req);
